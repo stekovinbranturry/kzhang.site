830f691af262ea021459a04f664e3340af95eb08
ZHANG Kai <kai.zhang@soprasteria.com>
2019-08-02 16:03:28 +0800

    version 1

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Modal Image Gallery
+function onClick(element) {
+	document.getElementById('img01').src = element.src;
+	document.getElementById('modal01').style.display = 'block';
+	var captionText = document.getElementById('caption');
+	captionText.innerHTML = element.alt;
+}
+
+// Change style of navbar on scroll
+window.onscroll = function() {
+	myFunction();
+};
+function myFunction() {
+	var navbar = document.getElementById('myNavbar');
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+		navbar.className = 'kzhang-bar' + ' kzhang-card' + ' kzhang-animate-top' + ' kzhang-white';
+	} else {
+		navbar.className = navbar.className.replace(' kzhang-card kzhang-animate-top kzhang-white', '');
+	}
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+	var x = document.getElementById('navDemo');
+	if (x.className.indexOf('kzhang-show') == -1) {
+		x.className += ' kzhang-show';
+	} else {
+		x.className = x.className.replace(' kzhang-show', '');
+	}
+}
